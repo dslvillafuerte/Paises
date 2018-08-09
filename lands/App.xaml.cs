2d014b8fe4be02fbdf3c5e056f2a32ed01285046
dslvillafuerte <lvillafuerte@digitalstrategy.com.ec>
2018-08-09 15:57:33 -0500

    V1

--- lands/App.xaml.cs
+++ lands/App.xaml.cs
@@ -5,13 +5,14 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace lands
 {
+    using Views;
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
---
+++ lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace lands.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
