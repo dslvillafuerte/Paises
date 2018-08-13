39709ceb1889c2b567344e777df6401f30c089c6
dslvillafuerte <lvillafuerte@digitalstrategy.com.ec>
2018-08-12 22:33:53 -0500

    Models created

--- lands/Models/Currency.cs
+++ lands/Models/Currency.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿
+
+
 namespace lands.Models
 {
+    using Newtonsoft.Json;
     public class Currency
     {
-        public Currency()
-        {
-        }
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
     }
 }
